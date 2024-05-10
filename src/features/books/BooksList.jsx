bcb3d2a1e14c5d6af51a9a03bfa17cdaf641a010
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-09 21:09:33 -0300

    show

--- src/features/books/BooksList.jsx
+++ src/features/books/BooksList.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 
-import React from 'react'
-
 const BooksList = () => {
   return (
     <div>BooksList</div>
+
   )
 }
 
 export default BooksList
+
+/* o que eu quero que apareça aqui no bookList?
+os livros com a foto do livro, nome do autor embaixo em itálico */
