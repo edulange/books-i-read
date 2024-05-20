69fcc920b4270b2b6591332c69e9de58148344e0
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-19 21:07:40 -0300

    useContext e ProtectedRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,38 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import BooksList from './features/books/BooksList'
-import { Routes, Route } from 'react-router-dom'
 import AddBookForm from './features/books/AddBookForm'
 import UsersList from './features/users/UsersList'
 import BookSearch from './features/books/BookSearch'
 import Cadastro from './components/Cadastro'
 import TabelaUsuarios from './components/TabelaUsuarios'
-import { useState } from 'react'
 import Login from './components/Login'
+import ProtectedRoute from './routes/ProtectedRoute'
+import { AuthProvider } from './hooks/useAuth'
 
 function App() {
-	const [atualizaTabela, setAtualizaTabela] = useState(false)
-	const [usuarioSelecionado, setUsuarioSelecionado] = useState(null)
-
-	function handleAtualizaTabela() {
-		setAtualizaTabela(!atualizaTabela)
-	}
-
 	return (
-		<Routes>
-			<Route path='/' element={<Layout />}>
-				<Route index element={<Login />} />
-				<Route  element={<BooksList />} />
-
-				<Route path='book'>
-					<Route index element={<AddBookForm />} />
+		<AuthProvider>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<Layout />
+						</ProtectedRoute>
+					}
+				>
+					<Route index element={<BooksList />} />
+					<Route path='book' element={<AddBookForm />} />
+					<Route path='cadastro' element={<Cadastro />} />
+					<Route path='tabelaUsuarios' element={<TabelaUsuarios />} />
+					<Route path='search' element={<BookSearch />} />
+					<Route path='user' element={<UsersList />} />
 				</Route>
-
-				<Route path='cadastro'>
-					<Route
-						index
-						element={
-							<Cadastro
-								onCadastrado={handleAtualizaTabela}
-								usuarioSelecionado={usuarioSelecionado}
-								setUsuarioSelecionado={setUsuarioSelecionado}
-							/>
-						}
-					/>
-				</Route>
-
-				<Route path='tabelaUsuarios'>
-					<Route
-						index
-						element={
-							<TabelaUsuarios
-								atualizaTabela={atualizaTabela}
-								onAtualizaTabela={handleAtualizaTabela}
-								setUsuarioSelecionado={setUsuarioSelecionado}
-							/>
-						}
-					/>
-				</Route>
-
-				<Route path='search'>
-					<Route index element={<BookSearch />} />
-				</Route>
-
-				<Route path='user'>
-					<Route index element={<UsersList />} />
-				</Route>
-			</Route>
-		</Routes>
+			</Routes>
+		</AuthProvider>
 	)
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
 const Login = () => {
   const [name, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const response = await axios.post('http://localhost:3001/api/login', { name, password });
       localStorage.setItem('token', response.data.token);
+      login(); // Atualiza o estado de autenticação
+      navigate('/'); // Redireciona para a página principal
       alert('Login bem-sucedido!');
     } catch (error) {
       alert('Erro ao fazer login. Verifique as credenciais e tente novamente.');
@@ -18,8 +24,18 @@ const Login = () => {
   return (
     <div>
       <h2>Login</h2>
-      <input type="text" value={name} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
